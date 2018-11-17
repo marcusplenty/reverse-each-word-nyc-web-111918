5e05efbdc3a0929d9e059db932331432cef6060a
capable-publisher-7225 <marcusplenty@gmail.com>
2018-11-17 22:35:59 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,5 @@
 def reverse_each_word(string)
 	arr1 = string.split()
-	arr2 = arr1.collect{|word|
-		word.reverse!
-	end
-	arr1.join(" ")
+	arr2 = arr1.collect{|word| word.reverse!}
+	arr2.join(" ")
 end 
